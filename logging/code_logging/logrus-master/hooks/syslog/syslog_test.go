fab84fa8cfa87f9353366e04609aadac1b41f363
Dasha123d <dmalahova2@gmail.com>
2026-05-07 00:21:59 +0300

    code_logging

---
+++ logging/code_logging/logrus-master/hooks/syslog/syslog_test.go
@@ -0,0 +1,31 @@
+//go:build !windows && !nacl && !plan9
+
+package syslog_test
+
+import (
+	"io"
+	"log/syslog"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	lsyslog "github.com/sirupsen/logrus/hooks/syslog"
+)
+
+func TestLocalhostAddAndPrint(t *testing.T) {
+	log := logrus.New()
+	log.SetOutput(io.Discard)
+	hook, err := lsyslog.NewSyslogHook("udp", "localhost:514", syslog.LOG_INFO, "")
+	if err != nil {
+		t.Errorf("Unable to connect to local syslog.")
+	}
+
+	log.Hooks.Add(hook)
+
+	for _, level := range hook.Levels() {
+		if len(log.Hooks[level]) != 1 {
+			t.Errorf("SyslogHook was not added. The length of log.Hooks[%v]: %v", level, len(log.Hooks[level]))
+		}
+	}
+
+	log.Info("Congratulations!")
+}
---
+++ logging/code_logging/logrus-master/logger_bench_test.go
@@ -0,0 +1,81 @@
+package logrus_test
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func BenchmarkDummyLogger(b *testing.B) {
+	nullf, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0o666)
+	if err != nil {
+		b.Fatalf("%v", err)
+	}
+	defer nullf.Close()
+	doLoggerBenchmark(b, nullf, &logrus.TextFormatter{DisableColors: true}, smallFields)
+}
+
+func BenchmarkDummyLoggerNoLock(b *testing.B) {
+	nullf, err := os.OpenFile(os.DevNull, os.O_WRONLY|os.O_APPEND, 0o666)
+	if err != nil {
+		b.Fatalf("%v", err)
+	}
+	defer nullf.Close()
+	doLoggerBenchmarkNoLock(b, nullf, &logrus.TextFormatter{DisableColors: true}, smallFields)
+}
+
+func doLoggerBenchmark(b *testing.B, out *os.File, formatter logrus.Formatter, fields logrus.Fields) {
+	logger := logrus.Logger{
+		Out:       out,
+		Level:     logrus.InfoLevel,
+		Formatter: formatter,
+	}
+
+	b.RunParallel(func(pb *testing.PB) {
+		entry := logger.WithFields(fields) // new entry per goroutine
+		for pb.Next() {
+			entry.Info("aaa")
+		}
+	})
+}
+
+func doLoggerBenchmarkNoLock(b *testing.B, out *os.File, formatter logrus.Formatter, fields logrus.Fields) {
+	logger := logrus.Logger{
+		Out:       out,
+		Level:     logrus.InfoLevel,
+		Formatter: formatter,
+	}
+	logger.SetNoLock()
+
+	b.RunParallel(func(pb *testing.PB) {
+		entry := logger.WithFields(fields) // new entry per goroutine
+		for pb.Next() {
+			entry.Info("aaa")
+		}
+	})
+}
+
+func BenchmarkLoggerJSONFormatter(b *testing.B) {
+	doLoggerBenchmarkWithFormatter(b, &logrus.JSONFormatter{})
+}
+
+func BenchmarkLoggerTextFormatter(b *testing.B) {
+	doLoggerBenchmarkWithFormatter(b, &logrus.TextFormatter{})
+}
+
+func doLoggerBenchmarkWithFormatter(b *testing.B, f logrus.Formatter) {
+	b.SetParallelism(100)
+	log := logrus.New()
+	log.Formatter = f
+	log.Out = io.Discard
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.
+				WithField("foo1", "bar1").
+				WithField("foo2", "bar2").
+				Info("this is a dummy log")
+		}
+	})
+}
